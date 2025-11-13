00689c8354f4e963d75b4fa077f66cc93eef9271
issa-ahmed <issa.ahmed@strathmore.edu>
2025-11-13 19:27:41 +0300

    Frontend - Log In page as well as composers  and migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(); 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
